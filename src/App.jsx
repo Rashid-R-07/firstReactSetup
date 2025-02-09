7d96f00d12b56a74008ea2a60b78a162d04fddd7
Your Name <you@example.com>
2025-02-09 16:40:42 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hi This is my second projects</p>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+         
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
